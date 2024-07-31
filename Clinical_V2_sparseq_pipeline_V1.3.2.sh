06483a2bd85a9053af342c39f28a38f3e2ec6830
Lauren Caldwell <laurencaldwell11@gmail.com>
2024-07-31 16:46:03 -0400

    Update Clinical_V2_sparseq_pipeline_V1.3.2.sh
    fix incorrect file names

--- Clinical_V2_sparseq_pipeline_V1.3.2.sh
+++ Clinical_V2_sparseq_pipeline_V1.3.2.sh
@@ -18,12 +18,12 @@ gunzip $run_folder/R1_files/*.gz
 
 echo "	*Running bowtie alignment and counting reads with HTseq...*"
 mkdir -p $run_folder/R1_files/samcounts
-for f in $run_folder/R1_files/*.fastq; do bowtie $resource_folder/bowtie_index_V1p2/bw1_sparsq_V1p2_amplicons_index $f $f.sam --best -v 3 -k 1 -m 1 -S; done && for x in $run_folder/R1_files/*.sam; do python3 -m HTSeq.scripts.count -f sam -t CDS $x $resource_folder/bowtie_index_V1p2/sparsq_V1p2_amplicons.gtf > $x.count.txt; done;
+for f in $run_folder/R1_files/*.fastq; do bowtie $resource_folder/bowtie_index/clinical_V2_sparsq_V1p2_amplicons $f $f.sam --best -v 3 -k 1 -m 1 -S; done && for x in $run_folder/R1_files/*.sam; do python3 -m HTSeq.scripts.count -f sam -t CDS $x $resource_folder/bowtie_index/clinical_V2_sparsq_V1p2_amplicons.gtf > $x.count.txt; done;
 mv $run_folder/R1_files/*.count.txt $run_folder/R1_files/samcounts
 
 echo "	*Running analysis...*"
 mkdir -p $run_folder/results
-python3 $resource_folder/sparseq_analysis_V1.3.2.py $run_folder $resource_folder
+python3 $resource_folder/Clinical_V2_sparseq_analysis_V1.3.2.py $run_folder $resource_folder
 
 echo "	*Zipping FASTQ files...*"
 gzip $run_folder/R1_files/*.fastq
